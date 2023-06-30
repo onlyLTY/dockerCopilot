1bd65f80eec82dfea5b03f8c4d7522491ffafaf1
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-06-30 21:15:53 +0800

    调整执行顺序

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,7 @@ chmod 0644 /etc/cron.d/mycron
 cron
 tail -f /var/log/cron.log &
 /startup.sh
-python /app/manage.py runserver \[::\]:12712 >> /app/run.log 2>&1 &
+touch /app/run.log
 tail -f /app/run.log &
+python /app/manage.py runserver \[::\]:12712 >> /app/run.log 2>&1 &
 python /app/manage.py check_update_command >> /var/log/cron.log 2>&1 &
