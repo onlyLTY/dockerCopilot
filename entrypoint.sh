70413ddeca8deed32b60747068bd8ca85fc4187c
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-06-30 21:18:03 +0800

    回滚

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,5 @@ chmod 0644 /etc/cron.d/mycron
 cron
 tail -f /var/log/cron.log &
 /startup.sh
-touch /app/run.log
-tail -f /app/run.log &
 python /app/manage.py runserver \[::\]:12712 >> /app/run.log 2>&1 &
 python /app/manage.py check_update_command >> /var/log/cron.log 2>&1 &
