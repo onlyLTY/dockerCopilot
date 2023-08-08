da18c2e9bd461c4e38eb45aa1420ce5513f95758
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2023-08-08 22:47:18 +0800

    fix: docker启动时无法读取参数

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,7 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
-	SecretKey    string
-	Account      string
-	AccessExpire int64 `json:",default=86000"`
+	SecretKey    string `json:",string"`
+	Account      string `json:",string"`
+	AccessExpire int64  `json:",default=86000"`
 }
