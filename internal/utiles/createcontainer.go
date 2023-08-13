69072951c2c35c8d8c5f4a7356fd14511b6716b8
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-08-13 17:33:54 +0800

    feat: 获取旧容器配置和新建容器

--- internal/utiles/createcontainer.go
+++ internal/utiles/createcontainer.go
@@ -1,18 +1,13 @@
 package utiles
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
 	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/svc"
 	myTypes "github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"strconv"
 )
 
 type configWrapper struct {
@@ -23,86 +18,35 @@ type configWrapper struct {
 
 func CreateContainer(ctx *svc.ServiceContext, oldName string, newName string, imageNameAndTag string) (myTypes.MsgResp, error) {
 	containers, err := GetContainerList(ctx)
-	jwtToken, endpointsId, err := GetNewJwt(ctx)
-	headers := make(map[string]string)
-	headers["Authorization"] = "Bearer " + jwtToken
-	containerID, err := findContainerIDByName(containers, oldName)
-	baseURL := domain + "/api/endpoints/" + endpointsId
-	url := baseURL + "/docker/containers/" + containerID + "/json"
-	req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
 	if err != nil {
-		log.Println("创建请求失败")
-		log.Fatal(err)
+		return myTypes.MsgResp{}, err
 	}
-	req.Header.Set("Authorization", "Bearer "+jwtToken)
-	resp, err := http.DefaultClient.Do(req)
+	containerID, err := findContainerIDByName(containers, oldName)
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	inspectedContainer, err := cli.ContainerInspect(context.TODO(), containerID)
 	if err != nil {
 		log.Println("获取容器信息失败")
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("读取响应体失败")
-		log.Fatal(err)
-	}
-
-	var inspectedContainer types.ContainerJSON
-	err = json.Unmarshal(data, &inspectedContainer)
-	if err != nil {
-		log.Println("解析响应体失败")
 		log.Fatal(err)
 	}
 
 	inspectedContainer.Config.Hostname = ""
 	inspectedContainer.Config.Image = imageNameAndTag
 	inspectedContainer.Image = imageNameAndTag
+
+	config := inspectedContainer.Config
+	hostConfig := inspectedContainer.HostConfig
 	networkingConfig := &network.NetworkingConfig{
 		EndpointsConfig: inspectedContainer.NetworkSettings.Networks,
 	}
-	body := configWrapper{
-		Config:           inspectedContainer.Config,
-		HostConfig:       inspectedContainer.HostConfig,
-		NetworkingConfig: networkingConfig,
-	}
-	postData, err := json.Marshal(body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	createURL := baseURL + "/docker/containers/create?name=" + newName
-	createReq, err := http.NewRequestWithContext(context.Background(), "POST", createURL, bytes.NewBuffer(postData))
-	if err != nil {
-		log.Fatal(err)
-	}
-	createReq.Header.Set("Authorization", "Bearer "+jwtToken)
-	createReq.Header.Set("Content-Type", "application/json")
-	createResp, err := http.DefaultClient.Do(createReq)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer createResp.Body.Close()
 
-	createData, err := ioutil.ReadAll(createResp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	containerName := newName
 
-	log.Println("Response from create:", string(createData))
-	var responseMsg myTypes.MsgResp
-	switch createResp.StatusCode {
-	case http.StatusOK:
-		responseMsg = myTypes.MsgResp{Status: "200", Msg: "容器创建成功"}
-	case http.StatusBadRequest:
-		responseMsg = myTypes.MsgResp{Status: "400", Msg: "请求错误请重试"}
-	case http.StatusNotFound:
-		responseMsg = myTypes.MsgResp{Status: "404", Msg: "没有找到这个镜像"}
-	case http.StatusConflict:
-		responseMsg = myTypes.MsgResp{Status: "409", Msg: "存在冲突"}
-	case http.StatusInternalServerError:
-		responseMsg = myTypes.MsgResp{Status: "500", Msg: "docker服务异常"}
-	default:
-		responseMsg = myTypes.MsgResp{Status: strconv.Itoa(createResp.StatusCode), Msg: "未知错误"}
+	_, err = cli.ContainerCreate(context.TODO(), config, hostConfig, networkingConfig, nil, containerName)
+	if err != nil {
+		return myTypes.MsgResp{Msg: err.Error()}, err
 	}
-	return responseMsg, nil
+	return myTypes.MsgResp{}, nil
 }
