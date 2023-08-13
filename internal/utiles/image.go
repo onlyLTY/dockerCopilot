10abb0a84ff4554115939ea98cf0f17b95e8f84c
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-08-13 14:49:31 +0800

    feat: 适配UGOS

--- internal/utiles/image.go
+++ internal/utiles/image.go
@@ -1,70 +1,70 @@
 package utiles
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/svc"
-	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
+	MyType "github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
 	"log"
-	"net/http"
 	"strings"
 )
 
-func GetImagesList(ctx *svc.ServiceContext) ([]types.Image, error) {
-	imagelistdata := []types.Image{}
-	params := map[string]string{
-		"all": "true",
-	}
-	jwt, endpointsId, err := GetNewJwt(ctx)
+func GetImagesList(ctx *svc.ServiceContext) ([]MyType.Image, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return imagelistdata, err
+		log.Fatalf("Unable to create docker client: %s", err)
 	}
-	url := domain + "/api/endpoints/" + endpointsId + "/docker/images/json"
-	req, err := http.NewRequest("GET", url, nil)
+	var imagesList []MyType.Image
+	dockerImages, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Unable to fetch docker images: %s", err)
 	}
-	req.Header.Add("Authorization", jwt)
-	query := req.URL.Query()
-	for k, v := range params {
-		query.Add(k, v)
-	}
-	req.URL.RawQuery = query.Encode()
 
-	client := &http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		return imagelistdata, err
+	for _, img := range dockerImages {
+		image := MyType.Image{
+			Containers:  img.Containers,
+			Created:     img.Created,
+			ID:          img.ID,
+			Labels:      img.Labels,
+			ParentID:    img.ParentID,
+			RepoDigests: img.RepoDigests,
+			RepoTags:    img.RepoTags,
+			SharedSize:  img.SharedSize,
+			Size:        img.Size,
+			VirtualSize: img.VirtualSize,
+			ImageName:   "",
+			ImageTag:    "",
+			States:      0,
+			SizeFormat:  "",
+		}
+		imagesList = append(imagesList, image)
 	}
-	defer response.Body.Close()
-
-	err = json.NewDecoder(response.Body).Decode(&imagelistdata)
+	//看不明白就不要看了，这内存反复地申请，如果你看明白了 给这改成指针吧，啥？我为啥不直接写指针，我懒癌犯了就这样，欢迎pr
+	imagesList, err = checkImageInUsed(ctx, splitImageNameAndTag(calculateImageSize(imagesList)))
 	if err != nil {
-		return imagelistdata, err
+		return imagesList, err
 	}
-	//看不明白就不要看了，这内存反复的申请，如果你看明白了 给这改成指针吧，啥？我为啥不直接写指针，我懒癌犯了就这样，欢迎pr
-	imagelistdata, err = checkImageInUsed(ctx, splitImageNameAndTag(calculateImageSize(imagelistdata)))
-	if err != nil {
-		return imagelistdata, err
-	}
-	return imagelistdata, nil
+	return imagesList, nil
 }
-func splitImageNameAndTag(imagesList []types.Image) []types.Image {
+
+func splitImageNameAndTag(imagesList []MyType.Image) []MyType.Image {
 	for i, image := range imagesList {
 		if len(image.RepoTags) != 0 {
-			imagesList[i].Image_Name = strings.Split(image.RepoTags[0], ":")[0]
-			imagesList[i].Image_Tag = strings.Split(image.RepoTags[0], ":")[1]
+			imagesList[i].ImageName = strings.Split(image.RepoTags[0], ":")[0]
+			imagesList[i].ImageTag = strings.Split(image.RepoTags[0], ":")[1]
 		} else if len(image.RepoDigests) != 0 {
-			imagesList[i].Image_Name = strings.Split(image.RepoDigests[0], "@")[0]
-			imagesList[i].Image_Tag = "None"
+			imagesList[i].ImageName = strings.Split(image.RepoDigests[0], "@")[0]
+			imagesList[i].ImageTag = "None"
 		} else {
-			imagesList[i].Image_Name = "None"
-			imagesList[i].Image_Tag = "None"
+			imagesList[i].ImageName = "None"
+			imagesList[i].ImageTag = "None"
 		}
 	}
 	return imagesList
 }
-func checkImageInUsed(svc *svc.ServiceContext, imagelist []types.Image) ([]types.Image, error) {
+func checkImageInUsed(svc *svc.ServiceContext, imagelist []MyType.Image) ([]MyType.Image, error) {
 	list, err := GetContainerList(svc)
 	if err != nil {
 		return imagelist, err
@@ -80,7 +80,7 @@ func checkImageInUsed(svc *svc.ServiceContext, imagelist []types.Image) ([]types
 	}
 	return imagelist, nil
 }
-func calculateImageSize(imagesList []types.Image) []types.Image {
+func calculateImageSize(imagesList []MyType.Image) []MyType.Image {
 	for i := range imagesList {
 		if imagesList[i].Size >= 1024*1024*1024 {
 			imagesList[i].SizeFormat = // Convert size to gigabytes
