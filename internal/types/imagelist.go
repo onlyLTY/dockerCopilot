55c14b7ab5a2b8cfd977e3377348a2619d8e0df9
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-08-13 14:52:33 +0800

    feat: 适配UGOS

--- internal/types/imagelist.go
+++ internal/types/imagelist.go
@@ -1,18 +1,18 @@
 package types
 
 type Image struct {
-	Containers  int               `json:"Containers"`
+	Containers  int64             `json:"Containers"`
 	Created     int64             `json:"Created"`
 	ID          string            `json:"Id"`
 	Labels      map[string]string `json:"Labels"`
 	ParentID    string            `json:"ParentId"`
 	RepoDigests []string          `json:"RepoDigests"`
 	RepoTags    []string          `json:"RepoTags"`
-	SharedSize  int               `json:"SharedSize"`
-	Size        int               `json:"Size"`
-	VirtualSize int               `json:"VirtualSize"`
-	Image_Name  string            `json:"image_name"`
-	Image_Tag   string            `json:"image_tag"`
+	SharedSize  int64             `json:"SharedSize"`
+	Size        int64             `json:"Size"`
+	VirtualSize int64             `json:"VirtualSize"`
+	ImageName   string            `json:"imageName"`
+	ImageTag    string            `json:"imageTag"`
 	States      int               `json:"States"`
 	SizeFormat  string            `json:"SizeFormat"`
 }
