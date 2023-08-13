28d238f0ad0ac9c6034f7187aab601a817191a70
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-08-13 21:34:37 +0800

    feat: 删除未使用的镜像

--- internal/utiles/removeimage.go
+++ internal/utiles/removeimage.go
@@ -1,54 +1,23 @@
 package utiles
 
 import (
-	"encoding/json"
+	"context"
+	dockerTypes "github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/svc"
-	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
-	"net/http"
-	"strconv"
-	"strings"
+	myTypes "github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
+	"log"
 )
 
-func RemoveImage(ctx *svc.ServiceContext, imageID string, force bool) (types.MsgResp, error) {
-	jwt, endpointsId, err := GetNewJwt(ctx)
+func RemoveImage(ctx *svc.ServiceContext, imageID string, force bool) (myTypes.MsgResp, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		return types.MsgResp{}, err
+		log.Fatalf("Failed to create Docker client: %s", err)
 	}
-	imageID = strings.Split(imageID, ":")[1]
-	url := domain + "/api/endpoints/" + endpointsId + "/docker/images/" + imageID
-	req, err := http.NewRequest("DELETE", url, nil)
+	_, err = cli.ImageRemove(context.Background(), imageID, dockerTypes.ImageRemoveOptions{Force: force})
 	if err != nil {
-		return types.MsgResp{}, err
+		return myTypes.MsgResp{Msg: err.Error()}, err
 	}
-	req.Header.Add("Authorization", jwt)
-	params := map[string]bool{
-		"force": force,
-	}
-	query := req.URL.Query()
-	for k, v := range params {
-		query.Add(k, strconv.FormatBool(v))
-	}
-	req.URL.RawQuery = query.Encode()
-	client := &http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		return types.MsgResp{}, nil
-	}
-	defer response.Body.Close()
-	resp := types.MsgResp{Status: strconv.Itoa(response.StatusCode), Msg: response.Status}
-	type ErrorResponse struct {
-		Message string `json:"message"`
-	}
-	if response.StatusCode != http.StatusOK {
-		// 对于其他状态码，我们尝试解析响应体中的JSON错误消息
-		errorResponse := ErrorResponse{}
-		err = json.NewDecoder(response.Body).Decode(&errorResponse)
-		if err != nil {
-			// 在此处处理JSON解码错误
-			return types.MsgResp{}, err
-		}
-		// 如果解析成功，将错误消息设置为resp的Msg字段
-		resp.Msg = errorResponse.Message
-	}
-	return resp, nil
+
+	return myTypes.MsgResp{}, nil
 }
