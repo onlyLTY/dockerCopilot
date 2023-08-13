f23cf46ba369d2249af9582c3dfe792d8accd955
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-08-13 21:16:35 +0800

    feat: 删除旧容器

--- internal/utiles/removecontainer.go
+++ internal/utiles/removecontainer.go
@@ -1,50 +1,28 @@
 package utiles
 
 import (
-	"encoding/json"
+	"context"
+	dockerTypes "github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/svc"
-	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
-	"net/http"
-	"strconv"
+	myTypes "github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
 )
 
-func RemoveContainer(ctx *svc.ServiceContext, name string) (types.MsgResp, error) {
+func RemoveContainer(ctx *svc.ServiceContext, name string) (myTypes.MsgResp, error) {
 	containers, err := GetContainerList(ctx)
 	if err != nil {
-		return types.MsgResp{}, err
+		return myTypes.MsgResp{}, err
 	}
-	jwt, endpointsId, err := GetNewJwt(ctx)
 	containerID, err := findContainerIDByName(containers, name)
+	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		return types.MsgResp{}, err
+		panic(err)
 	}
-	url := domain + "/api/endpoints/" + endpointsId + "/docker/containers/" + containerID + "?v=1"
-	req, err := http.NewRequest("DELETE", url, nil)
+	// 删除容器
+	err = cli.ContainerRemove(context.Background(), containerID, dockerTypes.ContainerRemoveOptions{})
 	if err != nil {
-		return types.MsgResp{}, err
+		return myTypes.MsgResp{Status: err.Error()}, err
 	}
-	req.Header.Add("Authorization", jwt)
-	client := &http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		return types.MsgResp{}, nil
-	}
-	defer response.Body.Close()
-	resp := types.MsgResp{Status: strconv.Itoa(response.StatusCode), Msg: response.Status}
-	type ErrorResponse struct {
-		Message string `json:"message"`
-	}
-	// 对于204不解析
-	if response.StatusCode != http.StatusNoContent {
-		// 对于其他状态码，我们尝试解析响应体中的JSON错误消息
-		errorResponse := ErrorResponse{}
-		err = json.NewDecoder(response.Body).Decode(&errorResponse)
-		if err != nil {
-			// 在此处处理JSON解码错误
-			return types.MsgResp{}, err
-		}
-		// 如果解析成功，将错误消息设置为resp的Msg字段
-		resp.Msg = errorResponse.Message
-	}
-	return resp, nil
+
+	return myTypes.MsgResp{}, nil
 }
