e0dcb8233b10e0c29172a94e9682340443da82d3
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2023-12-11 17:04:02 +0800

    feat: 获取版本api
    获取api

--- internal/handler/version/versionhandler.go
+++ internal/handler/version/versionhandler.go
@@ -8,14 +8,14 @@ import (
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
-func GetVersionsHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+func VersionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		l := version.NewGetVersionsLogic(r.Context(), svcCtx)
-		resp, err := l.GetVersions()
+		l := version.NewVersionLogic(r.Context(), svcCtx)
+		resp, err := l.Version()
 		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			httpx.WriteJson(w, resp.Code, resp)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, resp)
+			httpx.WriteJson(w, resp.Code, resp)
 		}
 	}
 }
---
+++ internal/logic/version/versionlogic.go
@@ -0,0 +1,36 @@
+package version
+
+import (
+	"context"
+	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/config"
+
+	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/svc"
+	"github.com/onlyLTY/oneKeyUpdate/UGREEN/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type VersionLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewVersionLogic(ctx context.Context, svcCtx *svc.ServiceContext) *VersionLogic {
+	return &VersionLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *VersionLogic) Version() (resp *types.Resp, err error) {
+	resp = &types.Resp{}
+	resp.Code = 200
+	resp.Msg = "success"
+	resp.Data = map[string]string{
+		"version":   config.Version,
+		"buildDate": config.BuildDate,
+	}
+	return resp, nil
+}
